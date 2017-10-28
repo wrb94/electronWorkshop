54a0f54041a42225eb7981eae8005f77aa89f06a
Jakub Wróblewski <jakub.wroblewski@nokia.com>
2017-10-28 12:56:36 +0200

    markdown works

--- 03markdown/app.js
+++ 03markdown/app.js
@@ -1,6 +1,23 @@
 const electron = require('electron');
+const marked = require('marked');
 const ipc = electron.ipcRenderer;
 
+const $ = selector => document.querySelector(selector);
+
+const rawMarkdown = $('.raw-markdown');
+const renderedHtml = $('.rendered-html');
+
 ipc.on('file-opened', (event, file, content) => {
-    console.log(content);
+    rawMarkdown.value = content;
+    renderMarkdownToHtml(content);
+});
+
+function renderMarkdownToHtml(markdown) {
+    const html = marked(markdown);
+    renderedHtml.innerHTML = html;
+}
+
+rawMarkdown.addEventListener('keyup', event => {
+    const content = event.target.value;
+    renderMarkdownToHtml(content);
 });
