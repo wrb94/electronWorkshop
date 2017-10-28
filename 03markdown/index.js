7e9534634f5dd44fae871d53c9863cb925e63008
Jakub Wróblewski <jakub.wroblewski@nokia.com>
2017-10-28 14:35:58 +0200

    added menu

--- 03markdown/index.js
+++ 03markdown/index.js
@@ -1,14 +1,18 @@
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, dialog, Menu } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
 let mainWindow;
+let content;
 
 app.on('window-all-closed', function () {
     app.quit();
 });
 
 app.on('ready', function () {
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
     mainWindow = new BrowserWindow({ width: 1024, height: 768 });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     require('devtron').install()
@@ -29,11 +33,12 @@ function openFile() {
     if (!files) return;
     const file = files[0];
     mainWindow.setTitle(file);
-    const content = fs.readFileSync(file).toString();
+    content = fs.readFileSync(file).toString();
     mainWindow.webContents.send('file-opened', file, content);
 }
 
 function saveFile(content, originalFilePath) {
+    if (!content) return;
     let filePath;
     if (!originalFilePath)
         filePath = dialog.showSaveDialog(mainWindow, {
@@ -56,3 +61,73 @@ module.exports = {
     openFile: openFile,
     saveFile: saveFile
 }
+
+const template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open...',
+                accelerator: 'CmdOrCtrl+O',
+                click() { openFile() }
+            },
+            {
+                label: 'Save...',
+                accelerator: 'CmdOrCtrl+S',
+                click() {
+                    saveFile(content);
+                }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'CmdOrCtrl+Z',
+                role: 'undo'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'CmdOrCtrl+X',
+                role: 'cut'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'CmdOrCtrl+V',
+                role: 'paste'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            }
+        ]
+    },
+    {
+        label: 'Developer',
+        submenu: [
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin'
+                    ? 'Alt+Command+I'
+                    : 'Ctrl+Shift+I',
+                click() { mainWindow.webContents.toggleDevTools() }
+            }
+        ]
+    }
+]
